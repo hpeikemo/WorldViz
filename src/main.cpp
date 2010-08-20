77d5a29d9746d670910836b3063f1dd818a0385f
Hans Petter Eikemo <hpeikemo@me.com>
2010-08-20 18:14:05 +0200

    Implemented world draw as a DisplayList

--- src/main.cpp
+++ src/main.cpp
@@ -46,7 +46,68 @@ public:
 
 };
 
+
+
+Vec3f coordinateToPoint(double x, double y) {   
+    float r  = 2.0f;
+    float p  = cos( (y/180.0f) *M_PI );
+    float pz = cos( (x/180.0f) *M_PI ) * r *p;
+    float px = sin( (x/180.0f) *M_PI ) * r *p;
+    float py = sin( (y/180.0f) *M_PI ) * r;
+    return Vec3f(px,py,pz);            
+    //    return Vec3f(x/50,y/50,0);            
+}
+
+
+void drawShapefile(shapefileData *data) {
+    
+    //Render Polygon Shapefile	
+	for(int i=0;i<data->polygons.size();i++) {        
+        glBegin(GL_LINE_LOOP); //glBegin(GL_POLYGON);
+		for(int j=0;j<data->polygons[i].points.size();j++) {    
+            Vec3f p = coordinateToPoint(data->polygons[i].points[j].x, data->polygons[i].points[j].y);
+            glVertex3f(p.x, p.y, p.z);
+		}		
+		glEnd();
+	}
+    
+    //Render Line Shapefile
+	for(int i=0;i<data->lines.size();i++) {		
+		glBegin(GL_LINE_STRIP);
+        
+		for(int j=0;j<data->lines[i].points.size();j++) {
+            Vec3f p = coordinateToPoint(data->lines[i].points[j].x, data->lines[i].points[j].y);
+            glVertex3f(p.x, p.y, p.z);
+        }		
+        
+		glEnd();
+	}    
+    
+    //Render Point Shapefile
+    glEnable(GL_POINT_SMOOTH) ;
+	glPointSize(5.0);
+    
+//    GLfloat color[] = { 1.0, 0.0, 0.0, 1.0 };
+//	glMaterialfv(GL_FRONT, GL_DIFFUSE, color );
+    glBegin(GL_POINTS);
+	for(int i=0;i<data->points.size();i++) {
+        Vec3f p = coordinateToPoint(data->points[i].x,data->points[i].y);
+        glVertex3f(p.x, p.y, p.z);
+	}	
+	glEnd();  
+    
+}
+
+
+
+
+
 shapefileData shapes;
+
+GLuint shapeDList;
+
+
+
 
 void BasicParticleApp::prepareSettings( Settings* settings ) {
     settings->setWindowSize( 1280 , 720 );
@@ -67,10 +128,16 @@ void loadShapes(string path) {
 void BasicParticleApp::setup() {    
     
   //loadShapefile( getResourcePath("world.shp") );
-    loadShapes( getResourcePath("places.shp") );    
+  //loadShapes( getResourcePath("places.shp") );    
   //loadShapefile( getResourcePath("TM_WORLD_BORDERS-0.3.shp") );
     loadShapes( getResourcePath("TM_WORLD_BORDERS_SIMPL-0.3.shp") );
-        
+    
+    shapeDList = glGenLists(1);
+    glNewList(shapeDList, GL_COMPILE);
+    drawShapefile( &shapes );
+    glEndList();
+
+    
 }
 
 
@@ -78,6 +145,7 @@ void BasicParticleApp::mouseDown( MouseEvent event ) {
 }
 
 void BasicParticleApp::keyDown( KeyEvent event ) {
+    
 	if( event.getChar() == 'f' )
 		setFullScreen( ! isFullScreen() );
 }
@@ -95,30 +163,12 @@ void BasicParticleApp::resize( int width, int height ) {
     glMatrixMode(GL_MODELVIEW);    
 }
 
-Vec3f coordinateToPoint(double x, double y) {   
-    float r  = 2.0f;
-    float p  = cos( (y/180.0f) *M_PI );
-    float pz = cos( (x/180.0f) *M_PI ) * r *p;
-    float px = sin( (x/180.0f) *M_PI ) * r *p;
-    float py = sin( (y/180.0f) *M_PI ) * r;
-    return Vec3f(px,py,pz);            
-//    return Vec3f(x/50,y/50,0);            
-}
 
 
 float orbitDegrees = 0.0f;
 
 void BasicParticleApp::draw() {
-    
-    GLfloat mat_ambient[] = { 1.0, 1.0, 1.0, 1.0 };
-    GLfloat mat_specular[] = { 1.0, 1.0, 1.0, 1.0 };
-    GLfloat mat_black[] = { 0.0, 0.0, 0.0, 1.0 };
-    GLfloat light_position[] = { 8.0, 8.0, 8.0, 1.0 };
-    GLfloat lm_ambient[] = { 0.2, 0.2, 0.2, 1.0 };
-
-    GLfloat torus_diffuse[] = { 0.7, 0.7, 0.0, 1.0 };
-    
-    
+            
     glClearColor(0.0, 0.0, 0.0, 0.0);
     glClearAccum(0.0, 0.0, 0.0, 0.0);
 
@@ -128,67 +178,31 @@ void BasicParticleApp::draw() {
     glLoadIdentity();
     
     orbitDegrees += 0.2f;
-    glRotatef(orbitDegrees, 0.f, 1.f, 0.f);/* orbit the Y axis */
+    glRotatef(orbitDegrees, 0.f, 1.f, 0.5f);
 
-    glMaterialfv(GL_FRONT, GL_AMBIENT, mat_ambient);
-    glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
-    glMaterialf(GL_FRONT, GL_SHININESS, 120.0);
+    GLfloat light_position[] = { 8.0, 8.0, 8.0, 1.0 };
     glLightfv(GL_LIGHT0, GL_POSITION, light_position);
-    glLightModelfv(GL_LIGHT_MODEL_AMBIENT, lm_ambient);
     
+    GLfloat lm_ambient[] = { 0.5, 0.5, 0.5, 1.0 };    
+    glLightModelfv(GL_LIGHT_MODEL_AMBIENT, lm_ambient);    
     glEnable(GL_LIGHTING);
     glEnable(GL_LIGHT0);
     glEnable(GL_DEPTH_TEST);
-    glShadeModel (GL_FLAT);
+    glShadeModel(GL_FLAT);
     //glShadeModel(GL_SMOOTH);
 
     
-    
+    GLfloat mat_black[] = { 0.0, 0.0, 0.0, 0.7 };
     glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_black);
-//    gl::drawSphere( Vec3f(0.0f, 0.0f, 0.0f), 1.99f, 40 );
+    glMaterialfv(GL_FRONT, GL_AMBIENT, mat_black);
+    gl::drawSphere( Vec3f(0.0f, 0.0f, 0.0f), 1.99f, 40 );
 
+    GLfloat torus_diffuse[] = { 0.7, 0.7, 0.6, 1.0 };
     glMaterialfv(GL_FRONT, GL_DIFFUSE, torus_diffuse);
 
-    //glDisable(GL_LIGHTING);
-    glColor3f(1.0,0.0, 0.0);
-
-    //Render Polygon Shapefile	
-	for(int i=0;i<shapes.polygons.size();i++) {        
-        glBegin(GL_LINE_LOOP); //glBegin(GL_POLYGON);
-		for(int j=0;j<shapes.polygons[i].points.size();j++) {    
-            Vec3f p = coordinateToPoint(shapes.polygons[i].points[j].x, shapes.polygons[i].points[j].y);
-            glVertex3f(p.x, p.y, p.z);
-		}		
-		glEnd();
-	}
-    
-    //Render Line Shapefile
-	glColor3f (0.0, 1.0, 0.0);
-	for(int i=0;i<shapes.lines.size();i++) {		
-		glBegin(GL_LINE_STRIP);
-
-		for(int j=0;j<shapes.lines[i].points.size();j++) {
-            //shapePoint pnt = shapes.lines[i].points[j];
-            Vec3f p = coordinateToPoint(shapes.lines[i].points[j].x, shapes.lines[i].points[j].y);
-            glVertex3f(p.x, p.y, p.z);
-        }		
-
-		glEnd();
-	}    
-
-    //Render Point Shapefile
-    glEnable(GL_POINT_SMOOTH) ;
-	glPointSize(5.0);
-    
-    GLfloat color[] = { 1.0, 0.0, 0.0, 1.0 };
-	glMaterialfv(GL_FRONT, GL_DIFFUSE, color );
-    glBegin(GL_POINTS);	
-	for(int i=0;i<shapes.points.size();i++) {
-        Vec3f p = coordinateToPoint(shapes.points[i].x,shapes.points[i].y);
-        glVertex3f(p.x, p.y, p.z);
-	}	
-	glEnd();    
-    
+        
+//    drawShapefile( &shapes );
+    glCallList(shapeDList);
         
     
     glPopMatrix();
